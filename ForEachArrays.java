cf3e8c94086159c7450e4178b7f5d52c6ea61851
AndrewS3420 <andrewsanchez1123@gmail.com>
2024-09-03 12:12:39 -0700

    ForEachArrays.java

--- ForEachArrays.java
+++ ForEachArrays.java
@@ -61,6 +61,29 @@ public class ForEachArrays {
         for (String aStr : myString) {
             System.out.println("An elment in myStrings[] is " + aStr);
 
+            // Let's reverse a string,
+            String myStrToReverse = "abcdefg";
+
+            // Output the chars of myStrToReverse
+            for (int i = 0; i < 7; i++) {
+                System.out.println("myStrToReverse[" + i + "] is: " + myStrToReverse.charAt(i));
+            }
+            System.out.println(" End of initial output.");
+
+            // Create the reveres string.
+            String reversedStr = "";
+            // Output reversedStr
+            System.out.println("reversedStr before reversion loop isL " + reversedStr);
+
+            for (int i = myStrToReverse.length() - 1; i > 6; i--) {
+                char charToAdd = myStrToReverse.charAt(i);
+                reversedStr = reversedStr.concat(Character.toString(charToAdd));
+
+                // Output reversedStr.
+                System.out.println("reversedStr after reversion loop is " + reversedStr);
+
+
+            }
         }
     }
 }
